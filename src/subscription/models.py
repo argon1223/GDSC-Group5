a02b33e9692af1b2f7ced7f98694b0233fdfac37
david20571015 <david20571015@gmail.com>
2021-05-15 17:31:26 +0800

    Add Subscription database

--- src/subscription/models.py
+++ src/subscription/models.py
@@ -1,3 +1,54 @@
+import re
+
 from django.db import models
 
-# Create your models here.
+CATEGORY = {
+    'video' : {
+        'youtube': r'.*www\.youtube\.com\/channel.*',
+        'bahamut': r'.*ani\.gamer\.com\.tw\/animeVideo.*',
+    },
+    'comic': {
+        # TODO: use better reg for exact url for individual page
+        'webtoon': r'.*www\.webtoons\.com.*',
+    },
+}
+         
+
+class Subscription(models.Model):
+    category = models.CharField(max_length=100)
+    platform = models.CharField(max_length=100)
+    url = models.URLField(blank=True)
+
+  
+def parse_url(url):
+    for category, platform_list in CATEGORY.items():
+        for platform, reg in platform_list.items():
+            if re.match(reg, url):
+                return category, platform
+
+    raise ValueError("Invalid url")
+
+
+def add_subscription(url):
+    try: 
+        category, platform = parse_url(url)
+
+        if len(Subscription.objects.filter(
+            category=category,
+            platform=platform,
+            url=url)) == 0:
+
+            Subscription.objects.create(
+                category=category,
+                platform=platform,
+                url=url,
+            )
+
+    except ValueError:
+        print("error")
+        # TODO: add alert for inappropriate input url
+        pass
+
+
+def delete_subscription(pk):
+    Subscription.objects.get(id=pk).delete()
--- src/subscription/views.py
+++ src/subscription/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import Subscription, add_subscription, delete_subscription
 
-# Create your views here.
+def add_subscription_button_onclick(request):
+    if request.method == 'POST':
+        add_subscription(request.POST.get('url'))
+
+    subscriptions = Subscription.objects.all()
+
+    context = {
+        'subscriptions': subscriptions,
+    }
+
+    return render(request, 'index.html', context)
+
+def delete_subscription_button_onclick(request, pk):
+    delete_subscription(pk)
+
+    subscriptions = Subscription.objects.all()
+
+    context = {
+        'subscriptions': subscriptions,
+    }
+
+    return render(request, 'index.html', context)
